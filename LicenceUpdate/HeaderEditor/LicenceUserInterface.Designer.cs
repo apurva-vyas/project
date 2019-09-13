32bee79e31caecc8be7ac1fb1fbd298f8ef24c3f
apurva-vyas <apurvanca@gmail.com>
2019-09-13 09:19:58 +0530

    Add files via upload

---
+++ LicenceUpdate/HeaderEditor/LicenceUserInterface.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace HeaderEditor
+{
+    partial class LicenceUserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Header = new System.Windows.Forms.Label();
+            this.Save = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Header
+            // 
+            this.Header.AutoSize = true;
+            this.Header.Location = new System.Drawing.Point(454, 70);
+            this.Header.Name = "Header";
+            this.Header.Size = new System.Drawing.Size(79, 13);
+            this.Header.TabIndex = 1;
+            this.Header.Text = "Current Header";
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(443, 247);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 2;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(418, 112);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(168, 96);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Header);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label Header;
+        private System.Windows.Forms.Button Save;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ LicenceUpdate/HeaderEditor/LicenceUserInterface.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HeaderEditor
+{
+    public partial class LicenceUserInterface : Form
+      {
+
+        public LicenceUserInterface()
+        {
+            InitializeComponent();
+           
+            List<string> header = new List<string> { $"Title :", "Version: ", "Date And Time Of Creation : " };
+            richTextBox1.Text = String.Join(Environment.NewLine, header); 
+
+        }
+       
+        
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            string EditText = richTextBox1.Text;
+        }
+    }
+}
